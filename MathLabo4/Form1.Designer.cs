4d0f5ae94c8b41a9b6af1d9315b39431123b3a9b
bidphil <bid_phil@hotmail.com>
2015-05-13 17:35:52 -0400

    J'ai adder le projet

---
+++ MathLabo4/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace MathLabo4
+{
+    partial class LoiNormal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBX_Moyenne = new System.Windows.Forms.TextBox();
+            this.TBX_ÉcartType = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TBX_Cas3_Resultat = new System.Windows.Forms.TextBox();
+            this.TBX_Cas2_Resultat = new System.Windows.Forms.TextBox();
+            this.TBX_Cas1_Resultat = new System.Windows.Forms.TextBox();
+            this.BTN_Cas3 = new System.Windows.Forms.Button();
+            this.BTN_Cas2 = new System.Windows.Forms.Button();
+            this.TBN_Cas1 = new System.Windows.Forms.Button();
+            this.TBX_Cas1_VarB = new System.Windows.Forms.TextBox();
+            this.TBX_Cas3_VarA = new System.Windows.Forms.TextBox();
+            this.TBX_Cas2_VarA = new System.Windows.Forms.TextBox();
+            this.TBX_Cas1_VarA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Moyenne :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Écart type :";
+            // 
+            // TBX_Moyenne
+            // 
+            this.TBX_Moyenne.Location = new System.Drawing.Point(69, 42);
+            this.TBX_Moyenne.Name = "TBX_Moyenne";
+            this.TBX_Moyenne.Size = new System.Drawing.Size(100, 20);
+            this.TBX_Moyenne.TabIndex = 1;
+            // 
+            // TBX_ÉcartType
+            // 
+            this.TBX_ÉcartType.Location = new System.Drawing.Point(69, 69);
+            this.TBX_ÉcartType.Name = "TBX_ÉcartType";
+            this.TBX_ÉcartType.Size = new System.Drawing.Size(100, 20);
+            this.TBX_ÉcartType.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TBX_Cas3_Resultat);
+            this.groupBox1.Controls.Add(this.TBX_Cas2_Resultat);
+            this.groupBox1.Controls.Add(this.TBX_Cas1_Resultat);
+            this.groupBox1.Controls.Add(this.BTN_Cas3);
+            this.groupBox1.Controls.Add(this.BTN_Cas2);
+            this.groupBox1.Controls.Add(this.TBN_Cas1);
+            this.groupBox1.Controls.Add(this.TBX_Cas1_VarB);
+            this.groupBox1.Controls.Add(this.TBX_Cas3_VarA);
+            this.groupBox1.Controls.Add(this.TBX_Cas2_VarA);
+            this.groupBox1.Controls.Add(this.TBX_Cas1_VarA);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(11, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 149);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Les cas de probabilité";
+            // 
+            // TBX_Cas3_Resultat
+            // 
+            this.TBX_Cas3_Resultat.Location = new System.Drawing.Point(132, 113);
+            this.TBX_Cas3_Resultat.Name = "TBX_Cas3_Resultat";
+            this.TBX_Cas3_Resultat.Size = new System.Drawing.Size(43, 20);
+            this.TBX_Cas3_Resultat.TabIndex = 9;
+            // 
+            // TBX_Cas2_Resultat
+            // 
+            this.TBX_Cas2_Resultat.Location = new System.Drawing.Point(132, 84);
+            this.TBX_Cas2_Resultat.Name = "TBX_Cas2_Resultat";
+            this.TBX_Cas2_Resultat.Size = new System.Drawing.Size(43, 20);
+            this.TBX_Cas2_Resultat.TabIndex = 8;
+            // 
+            // TBX_Cas1_Resultat
+            // 
+            this.TBX_Cas1_Resultat.Location = new System.Drawing.Point(181, 31);
+            this.TBX_Cas1_Resultat.Name = "TBX_Cas1_Resultat";
+            this.TBX_Cas1_Resultat.Size = new System.Drawing.Size(43, 20);
+            this.TBX_Cas1_Resultat.TabIndex = 7;
+            // 
+            // BTN_Cas3
+            // 
+            this.BTN_Cas3.Location = new System.Drawing.Point(105, 111);
+            this.BTN_Cas3.Name = "BTN_Cas3";
+            this.BTN_Cas3.Size = new System.Drawing.Size(21, 23);
+            this.BTN_Cas3.TabIndex = 12;
+            this.BTN_Cas3.Text = "=";
+            this.BTN_Cas3.UseVisualStyleBackColor = true;
+            this.BTN_Cas3.Click += new System.EventHandler(this.BTN_Cas3_Click);
+            // 
+            // BTN_Cas2
+            // 
+            this.BTN_Cas2.Location = new System.Drawing.Point(105, 82);
+            this.BTN_Cas2.Name = "BTN_Cas2";
+            this.BTN_Cas2.Size = new System.Drawing.Size(21, 23);
+            this.BTN_Cas2.TabIndex = 11;
+            this.BTN_Cas2.Text = "=";
+            this.BTN_Cas2.UseVisualStyleBackColor = true;
+            this.BTN_Cas2.Click += new System.EventHandler(this.BTN_Cas2_Click);
+            // 
+            // TBN_Cas1
+            // 
+            this.TBN_Cas1.Location = new System.Drawing.Point(154, 29);
+            this.TBN_Cas1.Name = "TBN_Cas1";
+            this.TBN_Cas1.Size = new System.Drawing.Size(21, 23);
+            this.TBN_Cas1.TabIndex = 10;
+            this.TBN_Cas1.Text = "=";
+            this.TBN_Cas1.UseVisualStyleBackColor = true;
+            this.TBN_Cas1.Click += new System.EventHandler(this.TBN_Cas1_Click);
+            // 
+            // TBX_Cas1_VarB
+            // 
+            this.TBX_Cas1_VarB.Location = new System.Drawing.Point(105, 31);
+            this.TBX_Cas1_VarB.Name = "TBX_Cas1_VarB";
+            this.TBX_Cas1_VarB.Size = new System.Drawing.Size(27, 20);
+            this.TBX_Cas1_VarB.TabIndex = 4;
+            // 
+            // TBX_Cas3_VarA
+            // 
+            this.TBX_Cas3_VarA.Location = new System.Drawing.Point(58, 113);
+            this.TBX_Cas3_VarA.Name = "TBX_Cas3_VarA";
+            this.TBX_Cas3_VarA.Size = new System.Drawing.Size(27, 20);
+            this.TBX_Cas3_VarA.TabIndex = 6;
+            // 
+            // TBX_Cas2_VarA
+            // 
+            this.TBX_Cas2_VarA.Location = new System.Drawing.Point(58, 84);
+            this.TBX_Cas2_VarA.Name = "TBX_Cas2_VarA";
+            this.TBX_Cas2_VarA.Size = new System.Drawing.Size(27, 20);
+            this.TBX_Cas2_VarA.TabIndex = 5;
+            // 
+            // TBX_Cas1_VarA
+            // 
+            this.TBX_Cas1_VarA.Location = new System.Drawing.Point(36, 31);
+            this.TBX_Cas1_VarA.Name = "TBX_Cas1_VarA";
+            this.TBX_Cas1_VarA.Size = new System.Drawing.Size(27, 20);
+            this.TBX_Cas1_VarA.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "< X <";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(91, 116);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = ")";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(91, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = ")";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(138, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = ")";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "P( X >";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "P( X < ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "P(";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(175, 42);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(117, 65);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "IMPORTANT\r\n-------------------------\r\nPour un nombre négatif\r\nEx utilisation: 45." +
+    "6-\r\net non : -45.6";
+            // 
+            // LoiNormal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 285);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TBX_ÉcartType);
+            this.Controls.Add(this.TBX_Moyenne);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "LoiNormal";
+            this.Text = "La loi normale";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBX_Moyenne;
+        private System.Windows.Forms.TextBox TBX_ÉcartType;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TBX_Cas3_Resultat;
+        private System.Windows.Forms.TextBox TBX_Cas2_Resultat;
+        private System.Windows.Forms.TextBox TBX_Cas1_Resultat;
+        private System.Windows.Forms.Button BTN_Cas3;
+        private System.Windows.Forms.Button BTN_Cas2;
+        private System.Windows.Forms.Button TBN_Cas1;
+        private System.Windows.Forms.TextBox TBX_Cas1_VarB;
+        private System.Windows.Forms.TextBox TBX_Cas3_VarA;
+        private System.Windows.Forms.TextBox TBX_Cas2_VarA;
+        private System.Windows.Forms.TextBox TBX_Cas1_VarA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ MathLabo4/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathLabo4
+{
+    public partial class LoiNormal : Form
+    {
+        public LoiNormal()
+        {
+            InitializeComponent();
+        }
+        public static readonly Double[,] Table = {
+{0.0000, 0.0398, 0.0793, 0.1179, 0.1554, 0.1915, 0.2257, 0.2580, 0.2881, 0.3159, 0.3413, 0.3643, 0.3849, 0.4032, 0.4192, 0.4332, 0.4452, 0.4554, 0.4641, 0.4713, 0.4772, 0.4821, 0.4861, 0.4893, 0.4918, 0.4938, 0.4953, 0.4965, 0.4974, 0.4981, 0.4987, 0.4990, 0.4993, 0.4995, 0.4997, 0.4998, 0.4998, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0040, 0.0438, 0.0832, 0.1217, 0.1591, 0.1950, 0.2291, 0.2611, 0.2910, 0.3186, 0.3438, 0.3665, 0.3869, 0.4049, 0.4207, 0.4345, 0.4463, 0.4564, 0.4649, 0.4719, 0.4778, 0.4826, 0.4864, 0.4896, 0.4920, 0.4940, 0.4955, 0.4966, 0.4975, 0.4982, 0.4987, 0.4991, 0.4993, 0.4995, 0.4997, 0.4998, 0.4998, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0080, 0.0478, 0.0871, 0.1255, 0.1628, 0.1985, 0.2324, 0.2642, 0.2939, 0.3212, 0.3461, 0.3686, 0.3888, 0.4066, 0.4222, 0.4357, 0.4474, 0.4573, 0.4656, 0.4726, 0.4783, 0.4830, 0.4868, 0.4898, 0.4922, 0.4941, 0.4956, 0.4967, 0.4976, 0.4982, 0.4987, 0.4991, 0.4994, 0.4995, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000}, 
+{0.0120, 0.0517, 0.0910, 0.1293, 0.1664, 0.2019, 0.2357, 0.2673, 0.2967, 0.3238, 0.3485, 0.3708, 0.3907, 0.4082, 0.4236, 0.4370, 0.4484, 0.4582, 0.4664, 0.4732, 0.4788, 0.4834, 0.4871, 0.4901, 0.4925, 0.4943, 0.4957, 0.4968, 0.4977, 0.4983, 0.4988, 0.4991, 0.4994, 0.4996, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0160, 0.0557, 0.0948, 0.1331, 0.1700, 0.2054, 0.2389, 0.2704, 0.2995, 0.3264, 0.3508, 0.3729, 0.3925, 0.4099, 0.4251, 0.4382, 0.4495, 0.4591, 0.4671, 0.4738, 0.4793, 0.4838, 0.4875, 0.4904, 0.4927, 0.4945, 0.4959, 0.4969, 0.4977, 0.4984, 0.4988, 0.4992, 0.4994, 0.4996, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0199, 0.0596, 0.0987, 0.1368, 0.1736, 0.2088, 0.2422, 0.2734, 0.3023, 0.3289, 0.3531, 0.3749, 0.3944, 0.4115, 0.4265, 0.4394, 0.4505, 0.4599, 0.4678, 0.4744, 0.4798, 0.4842, 0.4878, 0.4906, 0.4929, 0.4946, 0.4960, 0.4970, 0.4978, 0.4984, 0.4989, 0.4992, 0.4994, 0.4996, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0239, 0.0636, 0.1026, 0.1406, 0.1772, 0.2123, 0.2454, 0.2764, 0.3051, 0.3315, 0.3554, 0.3770, 0.3962, 0.4131, 0.4279, 0.4406, 0.4515, 0.4608, 0.4686, 0.4750, 0.4803, 0.4846, 0.4881, 0.4909, 0.4931, 0.4948, 0.4961, 0.4971, 0.4979, 0.4985, 0.4989, 0.4992, 0.4994, 0.4996, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0279, 0.0675, 0.1064, 0.1443, 0.1808, 0.2157, 0.2486, 0.2794, 0.3078, 0.3340, 0.3577, 0.3790, 0.3980, 0.4147, 0.4292, 0.4418, 0.4525, 0.4616, 0.4693, 0.4756, 0.4808, 0.4850, 0.4884, 0.4911, 0.4932, 0.4949, 0.4962, 0.4972, 0.4979, 0.4985, 0.4989, 0.4992, 0.4995, 0.4996, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0319, 0.0714, 0.1103, 0.1480, 0.1844, 0.2190, 0.2517, 0.2823, 0.3106, 0.3365, 0.3599, 0.3810, 0.3997, 0.4162, 0.4306, 0.4429, 0.4535, 0.4625, 0.4699, 0.4761, 0.4812, 0.4854, 0.4887, 0.4913, 0.4934, 0.4951, 0.4963, 0.4973, 0.4980, 0.4986, 0.4990, 0.4993, 0.4995, 0.4996, 0.4997, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000},
+{0.0359, 0.0753, 0.1141, 0.1517, 0.1879, 0.2224, 0.2549, 0.2852, 0.3133, 0.3389, 0.3621, 0.3830, 0.4015, 0.4177, 0.4319, 0.4441, 0.4545, 0.4633, 0.4706, 0.4767, 0.4817, 0.4857, 0.4890, 0.4916, 0.4936, 0.4952, 0.4964, 0.4974, 0.4981, 0.4986, 0.4990, 0.4993, 0.4995, 0.4997, 0.4998, 0.4998, 0.4999, 0.4999, 0.4999, 0.5000, 0.5000}
+};
+        // Variable pour chaque champs de text
+        public double moyenne;
+        public double écartType;
+
+        public double cas1VarA;
+        public double cas1VarB;
+        public double cas1Resultat;
+
+        public double cas2VarA;
+        public double cas2Resultat;
+
+        public double cas3VarA;
+        public double cas3Resultat;
+
+
+        // P( ? < Z < ? )
+        private void TBN_Cas1_Click(object sender, EventArgs e)
+        {
+            // Initialisation importante pour le fonctionnement du programme
+            cas1Resultat = 0;
+            VérifierXetO();
+            
+            double varA_Z;
+            double varB_Z;
+            double tempA;
+            double tempB;
+
+            // Mettre la valeur des textbox dans les variables
+            cas1VarA = System.Double.Parse(TBX_Cas1_VarA.Text, System.Globalization.NumberStyles.AllowDecimalPoint | NumberStyles.AllowTrailingSign, System.Globalization.NumberFormatInfo.InvariantInfo);
+            cas1VarB = System.Double.Parse(TBX_Cas1_VarB.Text, System.Globalization.NumberStyles.AllowDecimalPoint | NumberStyles.AllowTrailingSign, System.Globalization.NumberFormatInfo.InvariantInfo);
+
+            // Calculer la code Z pour chaque valeur
+            varA_Z = (cas1VarA - moyenne) / écartType;
+            varB_Z = (cas1VarB - moyenne) / écartType;
+
+            // Les arrondirs
+            varA_Z = Math.Round(varA_Z, 2);
+            varB_Z = Math.Round(varB_Z, 2);
+
+            // Rendre les cotes Z positives.
+            if(varA_Z < 0)
+            {
+                varA_Z = varA_Z * -1;
+            }
+            if (varB_Z < 0)
+            {
+                varB_Z = varB_Z * -1;
+            }
+
+            // Trouver le pourcentage de probabiliter avec chaque cote Z           
+            tempA = TrouverCoteZDansTable(varA_Z);// Sa retourne la P( 0 < Z < varA_Z )
+            tempB = TrouverCoteZDansTable(varB_Z);// P( 0 < Z < varB_Z )
+
+            // Toutes les conditions pour le fonctionnement de la loi normale
+            if ((cas1VarA > moyenne && cas1VarB > moyenne) || 
+                (cas1VarA < moyenne && cas1VarB < moyenne))
+                cas1Resultat = tempB - tempA;
+            else
+                cas1Resultat = tempA + tempB;
+
+            // Écrire le résultat dans le bon TextBox
+            TBX_Cas1_Resultat.Text = cas1Resultat.ToString();
+        }
+        private void VérifierXetO()
+        {
+            moyenne = System.Double.Parse(TBX_Moyenne.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo);
+            écartType = System.Double.Parse(TBX_ÉcartType.Text,System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo);
+        }
+        private double TrouverCoteZDansTable(double z)
+        {
+            int Valeur1;
+            int Valeur2;
+            String sZ = z.ToString();
+            if(sZ.Length != 4)
+                Valeur1 = 0;
+            else
+                Valeur1 = System.Int32.Parse(sZ.Substring(3));
+
+            Valeur2 = (int)(z * 10);
+
+
+            return Table[Valeur1, Valeur2];
+            /*if (cas1VarA > moyenne && cas1VarB > moyenne)
+            cas1Resultat += Table[Valeur1, Valeur2];
+            cas2Resultat += Table[Valeur1, Valeur2];
+            if(cas3VarA > moyenne)
+                cas3Resultat -= Table[Valeur1, Valeur2];
+            else
+                cas3Resultat += Table[Valeur1, Valeur2];*/
+        }
+
+        // P( X < ? )
+        private void BTN_Cas2_Click(object sender, EventArgs e)
+        {
+            // Initialisation importante pour le fonctionnement du programme
+            cas2Resultat = 0;
+            VérifierXetO();
+           
+            double varA_Z;
+            double tempA;
+
+            // Mettre la valeur des textbox dans les variables
+            cas2VarA = System.Double.Parse(TBX_Cas2_VarA.Text, System.Globalization.NumberStyles.AllowDecimalPoint | NumberStyles.AllowTrailingSign, System.Globalization.NumberFormatInfo.InvariantInfo);
+
+            // Calculer la code Z pour chaque valeur
+            varA_Z = (cas2VarA - moyenne) / écartType;
+
+            // Les arrondirs
+            varA_Z = Math.Round(varA_Z, 2);
+
+            // Rendre les cotes Z positives.
+            if (varA_Z < 0)
+            {
+                varA_Z = varA_Z * -1;
+
+                // Trouver le pourcentage de probabiliter avec chaque cote Z  
+                tempA = TrouverCoteZDansTable(varA_Z);
+
+                if (cas2VarA > moyenne)
+                    cas2Resultat = 0.5 - tempA;
+                else
+                    cas2Resultat = 0.5 + tempA;
+            }
+            else
+            {
+                // Trouver le pourcentage de probabiliter avec chaque cote Z  
+                tempA = TrouverCoteZDansTable(varA_Z);
+
+                if(cas3VarA < moyenne)
+                    cas2Resultat = 0.5 - tempA;
+                else
+                    cas2Resultat = tempA + 0.5;
+            }
+
+            // Écrire le résultat dans le bon TextBox
+            TBX_Cas2_Resultat.Text = cas2Resultat.ToString();
+        }
+
+        // P( X > ? )
+        private void BTN_Cas3_Click(object sender, EventArgs e)
+        {
+            // Initialisation importante pour le fonctionnement du programme
+            cas3Resultat = 0;
+            VérifierXetO();
+            
+            double varA_Z;
+            double tempA;
+
+            // Mettre la valeur des textbox dans les variables
+            cas3VarA = System.Double.Parse(TBX_Cas3_VarA.Text, System.Globalization.NumberStyles.AllowDecimalPoint | NumberStyles.AllowTrailingSign, System.Globalization.NumberFormatInfo.InvariantInfo);
+
+            // Calculer la code Z pour chaque valeur
+            varA_Z = (cas3VarA - moyenne) / écartType;
+
+            // Les arrondirs
+            varA_Z = Math.Round(varA_Z, 2);
+
+            // Rendre les cotes Z positives.
+            if (varA_Z < 0)
+            {
+                varA_Z = varA_Z * -1;
+            }
+
+            // Trouver le pourcentage de probabiliter avec chaque cote Z  
+            tempA = TrouverCoteZDansTable(varA_Z);
+
+            if (cas3VarA > moyenne)
+                cas3Resultat = 0.5 - tempA;
+            else
+                cas3Resultat = 0.5 + tempA;
+
+            // Écrire le résultat dans le bon TextBox
+            TBX_Cas3_Resultat.Text = cas3Resultat.ToString();
+        }
+    }
+
+} 
+
